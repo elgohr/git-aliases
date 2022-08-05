699bff914717f754f7623f6e38dff71d2fca4042
Lars Gohr <elgohr@users.noreply.github.com>
2022-08-05 21:52:58 +0200

    Add autoSetupRemote

--- set-aliases.sh
+++ set-aliases.sh
@@ -4,3 +4,5 @@ git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.dci duet-commit
+
+git config --global --add --bool push.autoSetupRemote true
